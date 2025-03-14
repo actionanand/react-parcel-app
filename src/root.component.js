022a5abf8be56df946da7331bac4c1a097741fb9
Anand raja <meetanandns@gmail.com>
2025-03-14 10:44:48 +0000

    button style

--- src/root.component.js
+++ src/root.component.js
@@ -10,6 +10,9 @@ export default function Root(props) {
   console.log('passed from Angular:', dialogData);
 
   const handleSave = formData => {
+    if (formData.title.length > 30) {
+      formData.title = formData.title.substring(0, 30) + '...';
+    }
     console.log('Form Data:', formData);
     props[dialogRefAccessor].dialogRef.close({ event: 'update', message: 'Updated from react parcel', data: formData });
   };
@@ -18,24 +21,29 @@ export default function Root(props) {
     <section>
       <h3 className={styles.title}> {dialogData.title} </h3>
       <FormComponent todo={dialogData.todo} onSave={handleSave} />
-      <button
-        type='button'
-        onClick={() =>
-          props[dialogRefAccessor].dialogRef.close({
-            event: 'cancel',
-            message: 'Cancelled from react parcel',
-            data: null,
-          })
-        }>
-        Cancel
-      </button>
-      <button
-        type='button'
-        onClick={() => {
-          document.querySelector('form').dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
-        }}>
-        Update
-      </button>
+
+      <div className={styles.buttonContainer}>
+        <button
+          type='button'
+          className={`${styles.button} ${styles.cancel}`}
+          onClick={() =>
+            props[dialogRefAccessor].dialogRef.close({
+              event: 'cancel',
+              message: 'Cancelled from react parcel',
+              data: null,
+            })
+          }>
+          Cancel
+        </button>
+        <button
+          type='button'
+          className={`${styles.button} ${styles.update}`}
+          onClick={() => {
+            document.querySelector('form').dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
+          }}>
+          Update
+        </button>
+      </div>
     </section>
   );
 }
